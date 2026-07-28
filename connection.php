b462fc4dc30a58075c36186bf18a11411e6a24e5
Lakshmi-ctr <laxmipnlaxmipn@gmail.com>
2026-07-28 15:26:57 +0530

    Updated portfolio

--- connection.php
+++ connection.php
@@ -1,14 +1,14 @@
 <?php
 
-$database = new mysqli(
-    "localhost",
-    "root",
-    "",
-    "edoc"
-);
+$servername = "sql303.infinityfree.com";
+$username = "if0_42504097";
+$password = "3dtk5gR7ixokETn";   // Your vPanel/MySQL password
+$dbname = "if0_42504097_edoc";
 
-if ($database->connect_error){
-    die("Connection failed: ".$database->connect_error);
+$database = new mysqli($servername, $username, $password, $dbname);
+
+if ($database->connect_error) {
+    die("Connection failed: " . $database->connect_error);
 }
 
 ?>

78614da073c45ace5a6ccffa2f23e936ad3c9099
Lakshmi-ctr <laxmipnlaxmipn@gmail.com>
2026-07-28 14:44:51 +0530

    updated

--- connection.php
+++ connection.php
@@ -1,8 +1,14 @@
 <?php
 
-    $database= new mysqli("localhost","laxmi","1234","edoc");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
+$database = new mysqli(
+    "localhost",
+    "root",
+    "",
+    "edoc"
+);
+
+if ($database->connect_error){
+    die("Connection failed: ".$database->connect_error);
+}
 
 ?>
